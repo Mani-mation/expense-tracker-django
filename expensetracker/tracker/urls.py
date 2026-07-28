2c767c620552333566bac6391d8885b9f03241c0
Mani-mation <manikyashrivastava@gmail.com>
2026-07-28 14:03:00 +0530

    Add files via upload

---
+++ expensetracker/tracker/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('add/', views.add_expense, name='add_expense'),
+    path('budget/', views.set_budget, name='set_budget'),
+]
