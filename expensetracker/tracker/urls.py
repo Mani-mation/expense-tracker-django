d60ed5f673ce0bdc078e8893b20dc3e7c0d39f71
Mani-mation <manikyashrivastava@gmail.com>
2026-07-28 14:05:38 +0530

    Delete expensetracker directory

--- expensetracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('add/', views.add_expense, name='add_expense'),
-    path('budget/', views.set_budget, name='set_budget'),
-]
